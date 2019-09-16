8f84a397c53ac2cfc740b8c303eb451dab46fbd4
Guillermo Latumahina <glatumahina@roc-dev.com>
2019-09-16 17:30:41 +0200

    Uitwerking van de exercise

---
+++ for_klas.php
@@ -0,0 +1,10 @@
+<?php
+
+$array = array("> Sjonnie de Wiel \n", "> Herman Kaal \n", "> Henk de Steen \n", "> Inge Kerkhoven \n","> Gert Kruiswijk \n");
+$length = count($array);
+
+for ($i = 0; $i < $length; $i++) {
+	print($array [$i]) ;
+}
+
+?>
